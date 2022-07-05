b0227a1a97893ee008d12f68f342106538436e0c
Subhra <98174577+Subhra2002@users.noreply.github.com>
2022-07-05 11:52:54 +0530

    Update Qn_01.py

--- Python Assignment 2/Qn_01.py
+++ Python Assignment 2/Qn_01.py
@@ -6,3 +6,11 @@ for i in range(4,0,-1):
     for j in range(1,i+1):
         print("*",end="")
     print("\n")
+
+# another method
+
+#   user=5
+# for i in range(user):
+#     print("*" * (i))
+# for j in reversed(range(user+1)):
+#     print("*" * (j))
