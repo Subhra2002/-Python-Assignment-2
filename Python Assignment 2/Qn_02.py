d730709bc0e28a6e1fde491c8500f65b35f4469f
Subhra <98174577+Subhra2002@users.noreply.github.com>
2022-07-06 10:23:14 +0530

    Update Qn_02.py

--- Python Assignment 2/Qn_02.py
+++ Python Assignment 2/Qn_02.py
@@ -2,3 +2,8 @@ name = input("Enter the word you want to revers: ")
 
 a = name[::-1]
 print(a)
+
+#another method 
+#user=(input("Enter: "))
+#or i in reversed(range(len(user))):
+           # print(user[i],end="")
